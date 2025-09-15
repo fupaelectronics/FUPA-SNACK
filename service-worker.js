74c0f0184a58054dff8cfcc523e8999dd0ad3a1a
fupaelectronics <fupaelectronics@gmail.com>
2025-09-16 05:49:05 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,6 @@ const urlsToCache = [
   '/index.html',
   '/karyawan.html',
   '/admin.html',
-  '/app.js',
   'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:FILL,GRAD@1,200',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap',
   'https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js',
