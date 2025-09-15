32fae3904eea30bc7e22ef63c817958dad0b8dc6
fupaelectronics <fupaelectronics@gmail.com>
2025-09-15 18:12:08 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,10 +5,14 @@ const urlsToCache = [
   '/karyawan.html',
   '/admin.html',
   '/app.js',
-  '/manifest.webmanifest'
+  'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:FILL,GRAD@1,200',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-storage-compat.js'
 ];
 
-// Install service worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -18,28 +22,14 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Fetch cached resources
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
-  );
-});
-
-// Update service worker
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
