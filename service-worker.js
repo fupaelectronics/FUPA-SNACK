865f586de8a54270c75370a2375838c6e9e0d999
fupaelectronics <fupaelectronics@gmail.com>
2025-09-16 12:23:03 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,9 +15,7 @@ const urlsToCache = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
