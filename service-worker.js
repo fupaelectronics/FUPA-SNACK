2dea5fafff796a97265a058421a6adbb7e7ae89e
fupaelectronics <fupaelectronics@gmail.com>
2025-09-20 12:25:03 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,27 +4,31 @@ const urlsToCache = [
   '/index.html',
   '/karyawan.html',
   '/admin.html',
-  '/app.js',
-  '/manifest.webmanifest'
+  'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:FILL,GRAD@1,200',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
+      .then(function(cache) {
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
+      .then(function(response) {
         if (response) {
           return response;
         }
         return fetch(event.request);
-      })
+      }
+    )
   );
 });
